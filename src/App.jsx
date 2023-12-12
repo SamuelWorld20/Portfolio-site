658f5243d5babd255e97be31a5f6a733ae6a24e7
Samuel Ejalonibu <90863438+SamuelWorld20@users.noreply.github.com>
2023-12-12 14:23:25 +0000

    customize site

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Samuel Ejalonibu",
+  title: "SoftWare Engineering enthusiast",
+  email: "ejalonibuoluwaseyi@gmail.com",
+  gitHub: "SamuelWorld20",
+  // instagram: "microsoft",
+  linkedIn: "samuel-ejalonibu",
+  hashnode: "Samuel20",
+  twitter: "sammyoluwaseyi_",
+  // youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
